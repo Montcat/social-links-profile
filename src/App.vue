6eff6e5f2567b30ea4ae7631ba84d84662e323b1
Montcat <montcat99@gmail.com>
2024-04-04 13:19:21 +0000

    new file:   README.md 	new file:   index.html 	new file:   jsconfig.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/base.css 	new file:   src/assets/images/avatar-jessica.jpeg 	new file:   src/assets/main.css 	new file:   src/main.js 	new file:   vite.config.js 	new file:   yarn.lock

---
+++ src/App.vue
@@ -0,0 +1,121 @@
+<template>
+	<main class="container">
+		<div class="profile-container">
+			<div class="description-container">
+				<div class="img-container">
+					<img :src="AvatarImage" alt="Avatar Image" class="profile-picture" />
+				</div>
+				<p class="name">Jessica Randall</p>
+				<p class="location">London, United Kingdom</p>
+				<p class="bio">"Front-end developer and avid reader."</p>
+			</div>
+			<ul class="links-container">
+				<li class="link-items" v-for="item in listItems">
+					<a href="#" class="link-item">
+						{{ item }}
+					</a>
+				</li>
+			</ul>
+		</div>
+	</main>
+</template>
+
+<script setup>
+	import { ref } from "vue";
+	import AvatarImage from "@/assets/images/avatar-jessica.jpeg";
+
+	const listItems = ref([
+		"GitHub",
+		"Frontend Mentor",
+		"LinkedIn",
+		"Twitter",
+		"Instagram",
+	]);
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		display: grid;
+		background-color: var(--ncl-off-black);
+		padding: 5rem 0;
+		height: 100%;
+
+		.profile-container {
+			margin: auto;
+			display: grid;
+			background-color: var(--ncl-dark-gray);
+			width: 22rem;
+			border-radius: 14px;
+			padding: 2rem;
+
+			.description-container {
+				margin: auto;
+				display: flex;
+				flex-direction: column;
+
+				.img-container {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					padding: 1rem;
+
+					.profile-picture {
+						height: 6rem;
+						border-radius: 50%;
+					}
+				}
+
+				.name {
+					text-align: center;
+					font-weight: 900;
+					font-size: 1.5rem;
+					padding-bottom: 0.5rem;
+				}
+
+				.location {
+					text-align: center;
+					color: var(--pcl-green);
+					padding-bottom: 1.5rem;
+					font-size: 0.9rem;
+				}
+
+				.bio {
+					text-align: center;
+					opacity: 0.7;
+					font-size: 0.8rem;
+					margin-bottom: 1rem;
+				}
+			}
+
+			.links-container {
+				list-style: none;
+				padding: 0;
+				margin: auto;
+				display: grid;
+				width: 100%;
+				gap: 1rem;
+
+				.link-item {
+					margin: auto;
+					width: 18rem;
+					height: 2.5rem;
+					border-radius: 8px;
+					background-color: var(--ncl-gray);
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					font-weight: 700;
+					text-decoration: none;
+					color: var(--ncl-white);
+					transition: 0.5s;
+				}
+
+				.link-item:hover {
+					background-color: var(--pcl-green);
+					text-decoration: none;
+					color: var(--ncl-off-black);
+				}
+			}
+		}
+	}
+</style>
